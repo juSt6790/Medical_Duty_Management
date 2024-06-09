c3f674291cd2072ac48d991ca8679ee2bbf163f7
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2024-06-09 09:29:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+function submitForm(userType) {
+            
+            window.location.href = 'login.html?userType=' + userType;
+}
+//   var formData = new FormData();
+//   formData.append('userType', userType);
+//   var content = document.querySelector('.desktop-1');
+//   content.classList.remove('desktop-1');
+//   content.classList.add('desktop-2');
+//   content.innerHTML=`<div class="wrapper-frame-a">
+//   <img class="frame-a-icon" alt="" src="background.jpg" />
+// </div>
+// <div class="frame-b"></div>
+// <div class="container">
+//   <div class="text-input">
+//     <div class="rectangle-a"></div>
+//     <div class="ellipse-shape">
+//       <h1 class="welcome-to-mdm">Welcome to MDM</h1>
+//       <img
+//         class="ellipse-shape-child"
+//         loading="lazy"
+//         alt=""
+//         src="MDM_logo.jpeg"
+//       />
+//     </div>
+//     <img
+//       class="text-input-child"
+//       loading="lazy"
+//       alt=""
+//       src="./public/rectangle-4161.svg"
+//     />
+
+//     <img
+//       class="text-input-item"
+//       loading="lazy"
+//       alt=""
+//       src="./public/rectangle-4162.svg"
+//     />
+//   </div>
+//   <div class="forgot-pass-frame">
+//     <div class="user-i-d-input">
+//       <div class="user-id">User ID</div>
+//       <input class="text-input-child" type="text" placeholder="User ID" /> 
+//       <input class="text-input-item" type="password" placeholder="Password" />
+
+//       <div class="password-input">
+//         <div class="password">Password</div>
+//       </div>
+//     </div>
+//   </div>
+  
+//   <div class="forgot-pass-btn" style="margin-top: -25px;">
+//     <div style="margin-left: 40px;">
+//     <button class="login">Log In</button>
+//     </div>
+//     <div style="margin-left: -90px; margin-top: 25px;" >
+//     <button class="forgot">Forgot Password</button>
+//     <button class="sign-up"><a href="register.html" class="reg">Sign Up</a></button>
+//     </div>   
+//   </div>
+// </div>`;
+  
+
+// }
